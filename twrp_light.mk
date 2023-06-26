49ce873ba3978de97c8e78269cf201e3954ff409
lopestorm <guillopes2002@gmail.com>
2023-06-26 17:44:11 -0300

    Update twrp_light.mk

--- twrp_light.mk
+++ twrp_light.mk
@@ -8,9 +8,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Inherit from light device
 $(call inherit-product, device/xiaomi/light/device.mk)
 
